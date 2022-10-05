ba6c0286359aae72476824ff4cc40064780de496
ridge <ic21b008@technikum-wien.at>
2022-10-05 20:40:22 +0200

    Initial Commit

---
+++ src/main/java/fhtw/bic/slm_summenrechner2/SlmSummenrechner2Application.java
@@ -0,0 +1,13 @@
+package fhtw.bic.slm_summenrechner2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmSummenrechner2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmSummenrechner2Application.class, args);
+    }
+
+}
---
+++ src/test/java/fhtw/bic/slm_summenrechner2/SlmSummenrechner2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.bic.slm_summenrechner2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmSummenrechner2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
